36df8841e1d595f24f6b65569dc492225dca2e50
Fajkowsky <dawidfajkowski@gmail.com>
2013-02-07 10:26:56 +0100

    Minor changes.

--- skysoccer/settings/default.py
+++ skysoccer/settings/default.py
@@ -2,4 +2,5 @@ def make_settings(settings):
     settings['project_name'] = 'SkySoccer'
     settings['dbhost'] = 'localhost'
     settings['dbport'] = 27017
+    settings['db_name'] = 'test'
     return settings
