262954ee1a3d0e28ce2a0baa7fd235f5b6753724
Dominik "Socek" Długajczyk <msocek@gmail.com>
2013-02-07 12:38:41 +0100

    Fixed tests.

--- skysoccer/test/index.py
+++ skysoccer/test/index.py
@@ -11,14 +11,29 @@ class IndexAppTest(AppTest):
 class IndexControllerTest(ControllerTest):
 
     def test_index(self):
+        from skysoccer.controllers.homepage import index_view
+
         db = self.request.registry['mongodb']
         db.users.insert({
             'name' : 's',
             'surname' : 'd',
             })
-        from skysoccer.controllers.homepage import index_view
         res = index_view(self.request)
 
+        self.assertTrue('players' in res.data)
         players = res.data['players']
         self.assertEqual(1, len(players))
         self.assertEqual('s d', players[0])
+
+        db.users.insert({
+            'name' : 's2',
+            'surname' : 'd2',
+            })
+
+        res = index_view(self.request)
+
+        self.assertTrue('players' in res.data)
+        players = res.data['players']
+        self.assertEqual(2, len(players))
+        self.assertEqual('s d', players[0])
+        self.assertEqual('s2 d2', players[1])
