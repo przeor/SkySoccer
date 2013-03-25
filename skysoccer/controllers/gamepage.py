5e2e9478190fae94f0babece23828fd1bb6700a1
Fajkowsky <dawidfajkowski@gmail.com>
2013-03-25 20:41:58 +0100

    In game displaying name of players.

--- skysoccer/controllers/gamepage.py
+++ skysoccer/controllers/gamepage.py
@@ -6,11 +6,21 @@ from skysoccer.models.match import Match
 def game_view(request):
     def set_initial_data():
         return {
-            "title": "Panel gry",            
+            "title": "Panel gry",
         }
+
+    def set_teams(team, request):
+        players = request.session['players']
+        players_in_team = []
+        for player in players:
+            if team in player[0]:
+                players_in_team.append(player[1])
+        return players_in_team
     #-------------------------------------------------------------------------
     data_for_template = set_initial_data()
     data_for_template['logged'] = request.session['logged']
-    print request.session['players']
+    data_for_template['team1'] = set_teams('d1', request)
+    data_for_template['team2'] = set_teams('d2', request)
+    # print request.session['players']
 
     return JinjaResponse(request, 'game.html', data_for_template)
