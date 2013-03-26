ddfdc4919701c9fc7e02cfebe1072b1eef47424e
Fajkowsky <dawidfajkowski@gmail.com>
2013-03-26 12:50:50 +0100

    Getting result from gamepage

--- skysoccer/controllers/gamepage.py
+++ skysoccer/controllers/gamepage.py
@@ -16,11 +16,15 @@ def game_view(request):
             if team in player[0]:
                 players_in_team.append(player[1])
         return players_in_team
+
+    def submit_score(request):
+        print request.POST.get('big_score_team1'), request.POST.get('big_score_team2')
     #-------------------------------------------------------------------------
     data_for_template = set_initial_data()
     data_for_template['logged'] = request.session['logged']
     data_for_template['team1'] = set_teams('d1', request)
     data_for_template['team2'] = set_teams('d2', request)
     # print request.session['players']
-
+    if request.POST.get('submit_end_game') == "submitting":
+        submit_score(request)
     return JinjaResponse(request, 'game.html', data_for_template)
--- skysoccer/models/match.py
+++ skysoccer/models/match.py
@@ -2,5 +2,6 @@ from mongoengine import Document, StringField
 
 
 class Match(Document):
+    number = StringField(max_length=5)
     name = StringField(max_length=120)
     surname = StringField(max_length=120)
