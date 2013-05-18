e10a0535e2d4acda78fe9cab0100b638cb19fee9
Fajkowsky <dawidfajkowski@gmail.com>
2013-05-18 14:33:08 +0200

    Scoring system done

--- skysoccer/controllers/gamepage.py
+++ skysoccer/controllers/gamepage.py
@@ -19,8 +19,16 @@ def game_view(request):
         return players_in_team
 
     def submit_score(request):
-        Match(number="1", score="1:2").save()
-        print request.POST.get('big_score_team1'), request.POST.get('big_score_team2')
+        score = [request.POST.get(
+            'big_score_team1'), request.POST.get('big_score_team2')]
+        if score[0] > score[1]:
+            win_team = data_for_template['team1']
+            lost_team = data_for_template['team2']
+        else:
+            win_team = data_for_template['team2']
+            lost_team = data_for_template['team1']
+
+        Match(score=score, win_team=win_team, lost_team=lost_team).save()
         return HTTPFound(location="/index2.html")
 
     #-------------------------------------------------------------------------
@@ -28,7 +36,7 @@ def game_view(request):
     data_for_template['logged'] = request.session['logged']
     data_for_template['team1'] = set_teams('d1', request)
     data_for_template['team2'] = set_teams('d2', request)
-    # print request.session['players']
+
     if request.POST.get('submit_end_game') == "submitting":
         return submit_score(request)
     return JinjaResponse(request, 'game.html', data_for_template)
--- skysoccer/models/match.py
+++ skysoccer/models/match.py
@@ -1,8 +1,13 @@
-from mongoengine import Document, StringField, DateTimeField
+from mongoengine import Document, StringField, DateTimeField, ListField
 import datetime
 
 
 class Match(Document):
-    number = StringField(max_length=5)
-    score = StringField(max_length=3)
+    number = StringField(max_length=3)
+    score = ListField()
     date = DateTimeField(default=datetime.datetime.now)
+    win_team = ListField()
+    lost_team = ListField()
+
+    def get_last_match_number(self):
+        pass
