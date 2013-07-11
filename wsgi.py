732962c92755a7611429cef94398058f5f1d3ab2
Fajkowsky <dawidfajkowski@gmail.com>
2013-07-11 09:02:51 +0200

    fix path

--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
 from pyramid.paster import get_app, setup_logging
-ini_path = '/home/faja/Pulpit/SkyGate/SkySoccer/frontend.ini'
+ini_path = 'frontend.ini'
 setup_logging(ini_path)
 application = get_app(ini_path, 'main')
